b4549918d1d3c66c396d63d5a90ab627ad2e9cdb
Dain Miller <carlton.dain.miller@gmail.com>
2020-04-20 05:39:51 -0400

    Add documentation on Notification domain object
    TL;DR:
    - Notifications are linked to recordings
    - Every time a recording is created we eavesdrop on recordings
    - Eavesdropping on notifications sends a notification to anyone who has signed up to be notified.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,10 @@
+# TODO: Implement the following...
+#
+# A `Notification` is linked to a Recording record.
+# 
+# Everytime there is a Recording record created, we 
+# eavesdrop on the recording and notify anyone who is
+# 'signed up' to receive notifications.
 class Notification
   include Notifiable
 end
