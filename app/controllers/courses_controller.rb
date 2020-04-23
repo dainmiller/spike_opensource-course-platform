f2e5706beb89485acdd74de47bce933245b55bca
Dain Miller <miller.dain@gmail.com>
2020-04-23 17:13:34 -0400

    Add note to remove API fetching from app/web servers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,8 +3,12 @@ class CoursesController < ApplicationController
   
   respond_to :html, :json
   
+  # TODO: Extract to an ActiveJob so I don't need to call this
+  # from the web layer, and can offset it to a diff set of worker
+  # servers - app servers not meant to pull in data from an API.
+  # Course.lazy_load_courses.all 
   def index
-    @courses ||= Course.lazy_load_courses.all
+    @courses ||= Course.all
   end
   
   def show
